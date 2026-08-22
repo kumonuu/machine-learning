20b79a66adb2f38b3aa7a78438ce56557caca78a
kumonuu <kumonuu@proton.me>
2026-08-22 13:00:33 +0100

    Add files via upload

--- machine-learning/covid_analysis.py
+++ machine-learning/covid_analysis.py
@@ -2,6 +2,7 @@ import plotly
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv("covid_data.csv")
 df = df.drop(columns=["Unnamed: 0","Admin2","FIPS","OBJECTID","Combined_Key"])
@@ -12,5 +13,22 @@ df["State"] = df["State"].fillna(value="")
 deaths = df.groupby("Country")["Deaths"].sum().nlargest(10)
 deaths = pd.DataFrame(deaths)
 
-figure = px.scatter(deaths,x=deaths.index,y="Deaths")
+bar = px.bar(deaths,x=deaths.index,y="Deaths",color=deaths.index,color_discrete_sequence=px.colors.qualitative.Set3,title="Top countries for COVID-19 deaths")
+bar.show()
+
+# figure = px.scatter(deaths,x=deaths.index,y="Deaths",color=deaths.index,size="Deaths")
+# figure.show()
+
+states = df[df["Country"] == "US"]
+figure = go.Figure(data=[
+    go.Bar(name="Confirmed Cases",x=states["Confirmed"],y=states["State"],orientation="h"),
+    go.Bar(name="Deaths",x=states["Deaths"],y=states["State"],orientation="h")
+])
+figure.update_layout(height=600)
 figure.show()
+
+# cases = states.nlargest(10,"Confirmed")
+# s_deaths = states.nlargest(10,"Deaths")
+
+# scatter = px.scatter(cases,x="Confirmed",y="Deaths",hover_name="State",title="Cases and deaths for top US states")
+# scatter.write_html("scatter.html")
